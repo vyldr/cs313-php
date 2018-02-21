4a2b7d0d98b1c5701a1aa07fd78df4ee4907760a
wildesteven <wil14043@byui.edu>
2018-02-20 21:26:38 -0700

    Added a create account page

---
+++ web/robot-control/create.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create an Account</title>
+</head>
+<body>
+    <form action="" method="post">
+        <table>
+            <tr>
+                <td><input type="text" name="user" placeholder="Username"></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td><input type="password" name="pass" id="" placeholder="Password"></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td><button type="submit">Create Account</button></td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
