e9397c3520c01150d01eee7e6af789333e0c776e
wildesteven <wil14043@byui.edu>
2018-02-20 19:06:32 -0700

    Added a basic sign in page

---
+++ web/robot-control/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign In</title>
+</head>
+<body>
+    <div>
+        <form action="" method="post">
+            <input type="text" name="user" placeholder="Username"><br>
+            <input type="password" name="pass" placeholder="Password"><br>
+            <button type="submit">Sign In</button>
+        </form>
+        <a href="create.php">Create an account</a>
+    </div>
+</body>
+</html>
