a3eb16c165e6508c4e79010c12e8d904df02cc63
wildesteven <wil14043@byui.edu>
2018-02-27 01:07:49 -0700

    THe robot list works now

--- web/robot-control/manage.php
+++ web/robot-control/manage.php
@@ -21,7 +21,7 @@ if (!isset($_SESSION["user"])) {
     <title>Manage</title>
 </head>
 <body>
-    <h1>Manage your Robots<a href="signOut.php"><?php echo $_SESSION["user"]; ?> Sign Out</a></h1>
+    <h1>Manage your Robots<a href="signOut.php"><?php echo $_SESSION["user"]; ?> - Sign Out</a></h1>
     <div id="managearea">
         <button onclick="addOne();" id="addone">Add one</button>
         <form action="addrobot.php" method="post" id="addform" style="display:none;">
@@ -32,10 +32,24 @@ if (!isset($_SESSION["user"])) {
             <tr>
                 <th>Name</th>
                 <th>Start</th>
-                <th>Remove</th>
             </tr>
             <?php // Insert the list of current robots from the database
-            echo "<tr><td>There is nothing here</td></tr>"
+
+                // Use the database
+                require("dbConnect.php");
+                $db = get_db();
+
+                // Prepare the statement
+                $user = $_SESSION["user"];
+                $query = "SELECT robot_name FROM lookup WHERE user_name = '$user'";
+                $statement = $db->prepare($query);
+                $statement->execute();
+
+                while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+                {
+                    echo "<tr><td>" . $row["robot_name"] . "</td><td><a href='control.php?robot=" . $row["robot_name"] . "'>Start</a></td></tr>";
+                }
+
             ?>
         </table>
     </div>
